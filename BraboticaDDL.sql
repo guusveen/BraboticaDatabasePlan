99e5f01993218b1ed9810e2aa0179f86d0f95d50
Guus Veen <guus_veen@hotmail.com>
2021-06-17 21:34:05 +0200

    Add files via upload

--- BraboticaDDL.sql
+++ BraboticaDDL.sql
@@ -3,9 +3,6 @@ Rol VARCHAR(255) NOT NULL,
 PRIMARY KEY (Rol)
 );
 
-INSERT INTO Rollen 
-Values ('Klant'), ('Medewerker'), ('Admin');
-
 CREATE TABLE Gebruikers (
 GebruikerId INT(10) NOT NULL AUTO_INCREMENT,
 Voornaam VARCHAR(255) NOT NULL,
@@ -14,6 +11,7 @@ Telefoonnummer VARCHAR(15),
 Email VARCHAR(255) NOT NULL,
 Rol VARCHAR(255) NOT NULL DEFAULT 'Klant',
 Wachtwoord VARCHAR(255) NOT NULL,
+VoorkeurTaal VARCHAR(10) DEFAULT 'NL',
 PRIMARY KEY (GebruikerId),
 FOREIGN KEY (Rol) REFERENCES Rollen(Rol),
 UNIQUE (Email)
@@ -63,6 +61,7 @@ Omschrijving VARCHAR(255) NOT NULL,
 CategorieId INT(10) NOT NULL,
 Prijs DECIMAL(10,2) NOT NULL,
 Voorraad INT DEFAULT 0,
+FotoAdres VARCHAR(255),
 PRIMARY KEY (ProductId),
 FOREIGN KEY (CategorieId) REFERENCES Categorieen(CategorieId)
 );
@@ -83,4 +82,11 @@ Aantal INT(10) NOT NULL,
 PRIMARY KEY (GebruikerId, ProductId),
 FOREIGN KEY (GebruikerId) REFERENCES Gebruikers(GebruikerId),
 FOREIGN KEY (ProductId) REFERENCES Producten(ProductId)
+);
+
+CREATE TABLE Zoektermen (
+ZoektermId INT(10) NOT NULL AUTO_INCREMENT,
+Zoekterm VARCHAR(255) NOT NULL,
+Zoekdatum DATETIME NOT NULL,
+PRIMARY KEY (ZoektermId)
 );
